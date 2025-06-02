f492e058f845ca7ac6e47649fc03dc3044c14bfe
mekh847 <mason.hayes@pulsecollegestudent.com>
2025-06-02 21:15:25 +0800

    Removed invalid nul file

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>FVRO Builder Web</h1>
+      <p>This is the start of your React Web version of the FVRO tool.</p>
+    </div>
+  );
+}
